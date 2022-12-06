fc2c813abdbcfda0338797dc4079f10c9d94057d
Bartek <ic20b087@technikum-wien.at>
2022-12-06 20:28:53 +0100

    Implementierung basic Logic ControllerImperial.java ServiceImperial.java

--- src/main/java/fhtw/imperialAPI/controller/ControllerImperial.java
+++ src/main/java/fhtw/imperialAPI/controller/ControllerImperial.java
@@ -1,5 +1,31 @@
 package fhtw.imperialAPI.controller;
 
+import fhtw.imperialAPI.dto.NumberImperial;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControllerImperial
 {
+
+    private NumberImperial service = new NumberImperial();
+
+
+    @GetMapping("/centimeter?inch/{zahl}")
+    int calcInch(@PathVariable int zahl)
+    {
+        //service.
+        //return service.
+        return 0;
+    }
+
+    @GetMapping("/meter?yard/{zahl}")
+    int calcYard(@PathVariable int zahl)
+    {
+        //service.
+        //return service.
+        return 0;
+    }
+
 }
--- src/main/java/fhtw/imperialAPI/service/ServiceImperial.java
+++ src/main/java/fhtw/imperialAPI/service/ServiceImperial.java
@@ -1,5 +1,18 @@
 package fhtw.imperialAPI.service;
 
+import fhtw.imperialAPI.dto.NumberImperial;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceImperial
 {
+    NumberImperial rechner = new NumberImperial();
+
+    public void setZahl(int newZahl) {rechner.setZahl(newZahl);}
+
+    public int getZahl() {return rechner.getZahl();}
+
+
+
+
 }
