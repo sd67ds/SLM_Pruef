ab9369498e4db0a7286d1e0342c20d3b89e509d4
Bartek <ic20b087@technikum-wien.at>
2022-12-06 20:20:22 +0100

    created DTO Erstellung der benötigten Variablen im DTO

--- src/main/java/fhtw/imperialAPI/dto/NumberImperial.java
+++ src/main/java/fhtw/imperialAPI/dto/NumberImperial.java
@@ -2,4 +2,44 @@ package fhtw.imperialAPI.dto;
 
 public class NumberImperial
 {
+    private int zahl;
+    private float inch;
+    private float yard;
+
+    public NumberImperial()
+    {
+        this.zahl = 0;
+        this.inch = 0;
+        this.yard = 0;
+    }
+
+    public int getZahl()
+    {
+        return zahl;
+    }
+
+    public void setZahl(int zahl)
+    {
+        this.zahl = zahl;
+    }
+
+    public float getInch()
+    {
+        return inch;
+    }
+
+    public void setInch(float inch)
+    {
+        this.inch = inch;
+    }
+
+    public float getYard()
+    {
+        return yard;
+    }
+
+    public void setYard(float yard)
+    {
+        this.yard = yard;
+    }
 }
